cef7fcc5068a2b2c5b2e1e064ec6c0e1c2da38be
cwza <cwz0205a@gmail.com>
2016-03-08 19:07:34 +0800

    refactoring URLs, views.py to make them tidier

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     url(r'^lists/the-only-list-in-the-world/$',
         views.view_list,
         name='view_list'),
+    url(r'^lists/new$',
+        views.new_list,
+        name='new_list'),
     # url(r'^admin/', include(admin.site.urls)),
 ]
