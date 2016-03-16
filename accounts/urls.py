3c39489cd722cb03f3ece55d03c3ba8b3feabea7
cwza <cwz0205a@gmail.com>
2016-03-16 17:11:26 +0800

    end of Chapter 16

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.views import logout
 from django.conf.urls import url
 from accounts import views
 
-urlpatterns = [url(r'^login$', views.persona_login, name='persona_login'), ]
+urlpatterns = [
+    url(r'^login$',
+        views.persona_login,
+        name='persona_login'),
+    url(r'^logout$',
+        logout,
+        {'next_page': '/'},
+        name='logout'),
+]
